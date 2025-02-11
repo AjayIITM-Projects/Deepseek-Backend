e2904738b46df91fd97af1aa51b2b25c2288d2f5
anujgupta95 <21f3001598@ds.study.iitm.ac.in>
2025-02-12 00:35:38 +0530

    added checkup routes & fixed

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 from mongoengine import connect, disconnect, get_db
 from datetime import datetime
 from api.controllers import *  # Import controllers
-from api.models import User, Course, Announcement, Week, Module, TestCase, Question, Test  # Import models
+from api.models import User, Course, Announcement, Week, Module, TestCase, Question  # Import models
 from dotenv import load_dotenv
 import os
 from api.seed_db import seed_database
